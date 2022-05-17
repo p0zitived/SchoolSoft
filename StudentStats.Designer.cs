ec56cb26b0853d374bf21f1824f9d83ea8620f47
p0zitive <danielhitrov13@gmail.com>
2022-05-17 19:50:32 +0300

    Am terminat cu rapoartele :DDDD

--- StudentStats.Designer.cs
+++ StudentStats.Designer.cs
@@ -30,13 +30,25 @@
         {
             this.components = new System.ComponentModel.Container();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource3 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource4 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.RestantieriBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.RestantieriBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.reportViewer2 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.reportViewer3 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.reportViewer4 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.Eminem8BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.SchoolDBDataSet = new SchoolSoft.SchoolDBDataSet();
+            this.GrupeAbsenteBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.RestantieriBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Eminem8BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SchoolDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GrupeAbsenteBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -73,7 +85,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
-            this.label4.Location = new System.Drawing.Point(1036, 9);
+            this.label4.Location = new System.Drawing.Point(1003, 9);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(240, 29);
             this.label4.TabIndex = 7;
@@ -91,11 +103,65 @@
             this.reportViewer1.Size = new System.Drawing.Size(314, 769);
             this.reportViewer1.TabIndex = 8;
             // 
+            // reportViewer2
+            // 
+            reportDataSource2.Name = "DataSet1";
+            reportDataSource2.Value = this.RestantieriBindingSource;
+            this.reportViewer2.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer2.LocalReport.ReportEmbeddedResource = "SchoolSoft.Reports.ReportEmin.rdlc";
+            this.reportViewer2.Location = new System.Drawing.Point(323, 41);
+            this.reportViewer2.Name = "reportViewer2";
+            this.reportViewer2.ServerReport.BearerToken = null;
+            this.reportViewer2.Size = new System.Drawing.Size(314, 769);
+            this.reportViewer2.TabIndex = 9;
+            // 
+            // reportViewer3
+            // 
+            reportDataSource3.Name = "DataSet1";
+            reportDataSource3.Value = this.Eminem8BindingSource;
+            this.reportViewer3.LocalReport.DataSources.Add(reportDataSource3);
+            this.reportViewer3.LocalReport.ReportEmbeddedResource = "SchoolSoft.Reports.Report8.rdlc";
+            this.reportViewer3.Location = new System.Drawing.Point(643, 41);
+            this.reportViewer3.Name = "reportViewer3";
+            this.reportViewer3.ServerReport.BearerToken = null;
+            this.reportViewer3.Size = new System.Drawing.Size(314, 769);
+            this.reportViewer3.TabIndex = 10;
+            // 
+            // reportViewer4
+            // 
+            reportDataSource4.Name = "DataSet1";
+            reportDataSource4.Value = this.GrupeAbsenteBindingSource;
+            this.reportViewer4.LocalReport.DataSources.Add(reportDataSource4);
+            this.reportViewer4.LocalReport.ReportEmbeddedResource = "SchoolSoft.Reports.ReportGroupsAbsences.rdlc";
+            this.reportViewer4.Location = new System.Drawing.Point(963, 41);
+            this.reportViewer4.Name = "reportViewer4";
+            this.reportViewer4.ServerReport.BearerToken = null;
+            this.reportViewer4.Size = new System.Drawing.Size(314, 769);
+            this.reportViewer4.TabIndex = 11;
+            // 
+            // Eminem8BindingSource
+            // 
+            this.Eminem8BindingSource.DataMember = "Eminem8";
+            this.Eminem8BindingSource.DataSource = this.SchoolDBDataSet;
+            // 
+            // SchoolDBDataSet
+            // 
+            this.SchoolDBDataSet.DataSetName = "SchoolDBDataSet";
+            this.SchoolDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // GrupeAbsenteBindingSource
+            // 
+            this.GrupeAbsenteBindingSource.DataMember = "GrupeAbsente";
+            this.GrupeAbsenteBindingSource.DataSource = this.SchoolDBDataSet;
+            // 
             // StudentStats
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1285, 810);
+            this.Controls.Add(this.reportViewer4);
+            this.Controls.Add(this.reportViewer3);
+            this.Controls.Add(this.reportViewer2);
             this.Controls.Add(this.reportViewer1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -108,6 +174,9 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.StudentStats_FormClosing);
             this.Load += new System.EventHandler(this.StudentStats_Load);
             ((System.ComponentModel.ISupportInitialize)(this.RestantieriBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Eminem8BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SchoolDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GrupeAbsenteBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,5 +189,11 @@
         private System.Windows.Forms.Label label4;
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
         private System.Windows.Forms.BindingSource RestantieriBindingSource;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer2;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer3;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer4;
+        private SchoolDBDataSet SchoolDBDataSet;
+        private System.Windows.Forms.BindingSource Eminem8BindingSource;
+        private System.Windows.Forms.BindingSource GrupeAbsenteBindingSource;
     }
 }
