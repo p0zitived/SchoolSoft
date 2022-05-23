3b72e8c6ad06766aa27bb5987b05cb16a33efd8e
unknown <danielhitrov13@gmail.com>
2022-05-23 12:54:24 +0300

    Am facut rapoartele frumoase

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -183,6 +183,16 @@ namespace SchoolSoft.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap hell {
+            get {
+                object obj = ResourceManager.GetObject("hell", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap KeyIconColor {
             get {
                 object obj = ResourceManager.GetObject("KeyIconColor", resourceCulture);
